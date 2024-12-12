d9a570e0b5b695d410e57a7e8788e6e01c3eebc6
Sinega-E <sinega.elanthiraiyan@gmail.com>
2024-12-12 16:57:20 +0530

    commit-1f

--- Project-2-Steps-Component/src/App v-1.js
+++ Project-2-Steps-Component/src/App v-1.js
@@ -9,7 +9,7 @@ const messages = [
 export default function App(){
     const [step, setStep]=useState(1);
     const [isOpen, setIsOpen]=useState(true);
-    function handlePrevious(){
+    function handleprevious(){
         if (step>1)setStep(s=>s - 1)
     }
 
@@ -30,20 +30,12 @@ export default function App(){
             Step {step}:{messages[step-1]}
         </p>
         <div className="buttons">
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>Previous<span>👈</span></Button>
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>Next<span>👉</span></Button>
-
+            <button style={{background:"#7950f2", color:"#fff"}} 
+            onClick={handleprevious}>Previous</button>
+            <button style={{background:"#7950f2", color:"#fff"}}
+            onClick={handleNext}>Next</button>
         </div>
     </div>)
     }
     </>)
-
-
-function Button({textColor, bgColor, onClick, children}){
-    return(
-        <button style={{background:bgColor, color:textColor}}
-        onClick={onClick}>{children}</button>
-    )
-}
-
     }
